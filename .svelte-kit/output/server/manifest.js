d5423792cd3c3a0dae02d66bf89bda02f992e45f
Osvaldo Castellanos <ocstllns@gmail.com>
2026-03-09 12:39:23 -0500

    updates

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set([".nojekyll","favicon.svg"]),
+	assets: new Set([".nojekyll","CNAME","favicon.svg"]),
 	mimeTypes: {".svg":"image/svg+xml"},
 	_: {
-		client: {start:"_app/immutable/entry/start.DuA4tC2P.js",app:"_app/immutable/entry/app.D12F7wmo.js",imports:["_app/immutable/entry/start.DuA4tC2P.js","_app/immutable/chunks/DxkPRLuP.js","_app/immutable/chunks/DQsj6ZHF.js","_app/immutable/chunks/BJy2OeFQ.js","_app/immutable/chunks/D0iwhpLH.js","_app/immutable/entry/app.D12F7wmo.js","_app/immutable/chunks/BoJMCFVt.js","_app/immutable/chunks/DQsj6ZHF.js","_app/immutable/chunks/CijGG5-r.js","_app/immutable/chunks/D_0qg-IN.js","_app/immutable/chunks/BJy2OeFQ.js","_app/immutable/chunks/DJHLDjRM.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.y-tU-rrr.js",app:"_app/immutable/entry/app.zbDnrF9h.js",imports:["_app/immutable/entry/start.y-tU-rrr.js","_app/immutable/chunks/B8F6qqY5.js","_app/immutable/chunks/DQsj6ZHF.js","_app/immutable/chunks/BJy2OeFQ.js","_app/immutable/chunks/D0iwhpLH.js","_app/immutable/entry/app.zbDnrF9h.js","_app/immutable/chunks/BoJMCFVt.js","_app/immutable/chunks/DQsj6ZHF.js","_app/immutable/chunks/CijGG5-r.js","_app/immutable/chunks/D_0qg-IN.js","_app/immutable/chunks/BJy2OeFQ.js","_app/immutable/chunks/DJHLDjRM.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
