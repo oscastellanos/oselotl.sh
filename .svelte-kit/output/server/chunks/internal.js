29f0fd62fa59204bbbfb032f689dba3c219579d1
Osvaldo Castellanos <ocstllns@gmail.com>
2026-03-09 12:59:02 -0500

    fixes

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -950,7 +950,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "reif0x"
+  version_hash: "6az5nd"
 };
 async function get_hooks() {
   let handle;
--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".nojekyll","CNAME","favicon.svg"]),
 	mimeTypes: {".svg":"image/svg+xml"},
 	_: {
-		client: {start:"_app/immutable/entry/start.Bp10aUdm.js",app:"_app/immutable/entry/app.CD5nZ3Ht.js",imports:["_app/immutable/entry/start.Bp10aUdm.js","_app/immutable/chunks/DaPQQEIp.js","_app/immutable/chunks/DQsj6ZHF.js","_app/immutable/chunks/BJy2OeFQ.js","_app/immutable/chunks/D0iwhpLH.js","_app/immutable/entry/app.CD5nZ3Ht.js","_app/immutable/chunks/BoJMCFVt.js","_app/immutable/chunks/DQsj6ZHF.js","_app/immutable/chunks/CijGG5-r.js","_app/immutable/chunks/D_0qg-IN.js","_app/immutable/chunks/BJy2OeFQ.js","_app/immutable/chunks/DJHLDjRM.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.6-xNTeXg.js",app:"_app/immutable/entry/app.Bwa4-YH-.js",imports:["_app/immutable/entry/start.6-xNTeXg.js","_app/immutable/chunks/Bt4D9MkJ.js","_app/immutable/chunks/DQsj6ZHF.js","_app/immutable/chunks/BJy2OeFQ.js","_app/immutable/chunks/D0iwhpLH.js","_app/immutable/entry/app.Bwa4-YH-.js","_app/immutable/chunks/BoJMCFVt.js","_app/immutable/chunks/DQsj6ZHF.js","_app/immutable/chunks/CijGG5-r.js","_app/immutable/chunks/D_0qg-IN.js","_app/immutable/chunks/BJy2OeFQ.js","_app/immutable/chunks/DJHLDjRM.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
